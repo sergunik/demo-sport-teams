e3433a1c7a792dc2739daa6efd68bca9072fdbc4
Serhii Topolnytskyi <sergunik@gmail.com>
2019-01-02 14:36:59 +0200

    Migrated players (Migration, model, resource, controller)

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Player;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Player  $players
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $players)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Player  $players
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Player $players)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Player  $players
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $players)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Player  $players
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $players)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::resource('team', 'TeamController');
+Route::resource('player', 'PlayerController');
